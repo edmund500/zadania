a3b6fdd87fe985f04d5fd965bd8c56aa75717a43
Mateusz Biczyk <Mateusz Biczyk>
2016-12-29 19:53:13 +0100

    zadanie

---
+++ konwerter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace temperatury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Jakiej konwersji chcesz dokonać?: 1 - Celsjusz na Fahrenheit, 2 Fahrenheit na Celsjusz");
+            Console.WriteLine("Podaj ilość stopni Celsjusza");
+            double stopnieC = Convert.ToDouble(Console.ReadLine());
+            double wynik = 9d/5d * stopnieC + 32;
+            Console.WriteLine(stopnieC + " stopni Celsjusza to " + wynik + " stopni Fahrenheita\n");
+
+            Console.WriteLine("Podaj ilość stopni Fahrenheita");
+            double stopnieF = Convert.ToDouble(Console.ReadLine());
+            double wynik2 = 5d / 9d * (stopnieF - 32);
+            Console.WriteLine(stopnieF + " stopni Fahrenheita to " + wynik2 + " stopni Celsjusza");
+            Console.ReadKey();
+        }
+    }
+}
